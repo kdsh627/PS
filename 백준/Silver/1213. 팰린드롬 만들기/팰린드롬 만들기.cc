05dff6eb8b968b4fa81c70f12c8cf8fe8454dd93
kdsh627 <105626103+kdsh627@users.noreply.github.com>
2023-07-29 11:57:14 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include <numeric>
+using namespace std;
+
+int check[26];
+int main() {
+	string s;
+	string result;
+	cin >> s;
+	for (auto a : s) {
+		check[a - 'A']++;
+	}
+	int odd = -1;
+	for (int i = 0; i < 26; i++) {
+		if (check[i] % 2 == 1) odd = i;
+		for (int j = 0; j < check[i]/2; j++) {
+			result.push_back((char)(i + 65));
+		}
+	}
+	if (odd != -1) {
+		result.push_back((char)(odd + 65));
+		check[odd]--;
+	}
+	for (int i = 25; i >= 0; i--) {
+		if (check[i] % 2 == 1) {
+			cout << "I'm Sorry Hansoo";
+			return 0;
+		}
+		for (int j = 0; j < check[i] / 2; j++) {
+			result.push_back((char)(i + 65));
+		}
+	}
+
+	cout << result;
+	return 0;
+}
