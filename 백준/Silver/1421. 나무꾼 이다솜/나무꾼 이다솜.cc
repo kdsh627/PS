3a6d8c5104cb15d3f65fa66780a91969e049c4d0
kdsh627 <105626103+kdsh627@users.noreply.github.com>
2023-07-29 09:34:44 +0900

    [Silver II] Title: 나무꾼 이다솜, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1421. 나무꾼 이다솜/나무꾼 이다솜.cc
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+long long money;
+long long num2;// 갱신이 진행될 변수
+int N, C, W;
+long long count;
+long long max;
+vector<int> v;
+void binary_search(int start, int end) {
+	for (int i = start; i <= end; i++) {
+		money = 0;
+		num2 = 0;
+		for (int a : v) {
+			int div = a / i;
+			num2 = div * W * i;
+			if (a % i == 0) {
+				::count = (div - 1) * C;
+			}
+			else {
+				::count = div * C;
+			}
+			num2 -= ::count;
+			if (num2 > 0) {
+				money += num2;
+			}
+			::max = ::max > money ? ::max : money;
+		}
+	}
+}
+
+int main() {
+	cin >> N >> C >> W;
+	int wood;
+	for (int i = 0; i < N; i++) {
+		cin >> wood;
+		v.push_back(wood);
+	}
+	sort(v.begin(), v.end());
+	binary_search(1, v.back());
+	cout << ::max;
+	return 0;
+}
